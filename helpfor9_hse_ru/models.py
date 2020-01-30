7bb93bf56a54728c883583dcbb0e0b64d2da68f2
Evgeny <startsev2000@outlook.com>
2020-01-30 23:58:04 +0300

    Фикс неполадок с прошлого коммита

--- helpfor9_hse_ru/models.py
+++ helpfor9_hse_ru/models.py
@@ -1,61 +1,55 @@
 from django.db import models
 
+
 class Administrator(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     job = models.CharField(max_length=200)
+
 
 class Facultative(models.Model):
     name = models.CharField(max_length=200)
     info = models.CharField(max_length=200)
     teacher = models.CharField(max_length=200)
 
-    dep1 = 'В'; dep2 = 'E'; dep3 = 'FL'
-    dep4 = 'C'; dep5 = 'RaP'; dep6 = 'H'
-    dep7 = 'HoC'; dep8 = 'M'; dep9 = 'WAC'
-    dep10 = 'SC'; dep11 = 'PS'; dep12 = 'L'
-    dep13 = 'T'; dep14 = 'PE'
+    DEPARTMENTS = [
+        ('Востоковедение', 'Востоковедение'),
+        ('Естественные науки', 'Естественные науки'),
+        ('Иностранные языки', 'Иностранные языки'),
+        ('Информатика', 'Информатика'),
+        ('Исследовательская и проектная деятельность',
+         'Исследовательская и проектная деятельность'),
+        ('История', 'История'),
+        ('История культуры', 'История культуры'),
+        ('Математика', 'Математика'),
+        ('МХК', 'МХК'),
+        ('Общественные науки', 'Общественные науки'),
+        ('Психология', 'Психология'),
+        ('Словесность', 'Словесность'),
+        ('ТОК', 'ТОК'),
+        ('Физическая культура', 'Физическая культура')
+    ]
 
-    DEPARTMENTS =  [        #Список кафедр
-            (dep1, 'Востоковедение'),
-            (dep2, 'Естественные науки'),
-            (dep3, 'Иностранные языки'),
-            (dep4, 'Информатика'),
-            (dep5, 'Исследовательская и проектная деятельность'),
-            (dep6, 'История'),
-            (dep7, 'История культуры'),
-            (dep8, 'Математика'),
-            (dep9, 'МХК'),
-            (dep10, 'Общественные науки'),
-            (dep11, 'Психология'),
-            (dep12, 'Словесность'),
-            (dep13, 'ТОК'),
-            (dep14, 'Физическая культура')
-        ]
-    
-    department = models.CharField(max_length=3, choices=DEPARTMENTS, default=0)
+    department = models.CharField(
+        max_length=50, choices=DEPARTMENTS, default=0)
 
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    date = models.DateField()
-    time = models.TimeField()
-    place = models.CharField(max_length=30)
+    info = models.CharField(max_length=200)
 
-    cour1 = 'В'; cour2 = 'Г'; cour3 = 'Д'; cour4 = 'Е'; cour5 = 'М' 
-    cour6 = 'И'; cour7 = 'П'; cour8 = 'Э'; cour9 = 'С'; cour10 = 'Ю' 
-    
-    COURSES = [         #список направлений
-        (cour1, 'Востоковедение'),
-        (cour2, 'Гуманитарные науки'),
-        (cour3, 'Дизайн'),
-        (cour4, 'Естественные науки'),
-        (cour5, 'Математика'),
-        (cour6, 'Информатика, инженерия и математика'),
-        (cour7, 'Психология'),
-        (cour8, 'Экономика и математика'),
-        (cour9, 'Экономика и социальные науки'),
-        (cour10, 'Юриспруденция')
+    COURSES = [
+        ('Востоковедение', 'Востоковедение'),
+        ('Гуманитарные науки', 'Гуманитарные науки'),
+        ('Дизайн', 'Дизайн'),
+        ('Естественные науки', 'Естественные науки'),
+        ('Математика', 'Математика'),
+        ('Информатика, инженерия и математика',
+         'Информатика, инженерия и математика'),
+        ('Психология', 'Психология'),
+        ('Экономика и математика', 'Экономика и математика'),
+        ('Экономика и социальные науки', 'Экономика и социальные науки'),
+        ('Юриспруденция', 'Юриспруденция')
     ]
 
-    course = models.CharField(max_length=1, choices=COURSES, default=0)
+    course = models.CharField(max_length=50, choices=COURSES, default=0)
--- helpfor9_hse_ru/views.py
+++ helpfor9_hse_ru/views.py
@@ -2,146 +2,224 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event, Facultative
 
+
 def index(request):
     return render(request, 'index.html')
+
 
 def calendar(request):
     return render(request, 'calendar.html')
 
+
 def calculator(request):
     return render(request, 'calculator.html')
 
+
 def calculator_1(request):
-    return render(request, 'calculator.html') #Востоковедение
+    return render(request, 'calculator.html')  # Востоковедение
+
 
 def calculator_2(request):
-    return render(request, 'calculator.html') #Гуманитарные науки
+    return render(request, 'calculator.html')  # Гуманитарные науки
+
 
 def calculator_3(request):
-    return render(request, 'calculator.html') #Дизайн
+    return render(request, 'calculator.html')  # Дизайн
+
 
 def calculator_4(request):
-    return render(request, 'calculator.html') #Естественные науки
+    return render(request, 'calculator.html')  # Естественные науки
+
 
 def calculator_5(request):
-    return render(request, 'calculator.html') #Математика
+    return render(request, 'calculator.html')  # Математика
+
 
 def calculator_6(request):
-    return render(request, 'calculator.html') #Информатика, инженерия и математика
+    # Информатика, инженерия и математика
+    return render(request, 'calculator.html')
+
 
 def calculator_7(request):
-    return render(request, 'calculator.html') #Психология
+    return render(request, 'calculator.html')  # Психология
+
 
 def calculator_8(request):
-    return render(request, 'calculator.html') #Экономика и математика
+    return render(request, 'calculator.html')  # Экономика и математика
+
 
 def calculator_9(request):
-    return render(request, 'calculator.html') #Экономика и социальные науки
+    return render(request, 'calculator.html')  # Экономика и социальные науки
+
 
 def calculator_10(request):
-    return render(request, 'calculator.html') #Юриспруденция
+    return render(request, 'calculator.html')  # Юриспруденция
+
 
 def info_1(request):
     return render(request, 'info1.html')
 
+
 def info_2(request):
     return render(request, 'info2.html')
+
 
 def info_3(request):
     return render(request, 'info3.html')
 
+
 def info_4(request):
     return render(request, 'info4.html')
+
 
 def info_5(request):
     return render(request, 'info5.html')
 
+
 def info_6(request):
     return render(request, 'info6.html')
+
 
 def info_7(request):
     return render(request, 'info7.html')
 
+
 def info_8(request):
     return render(request, 'info8.html')
+
 
 def info_9(request):
     return render(request, 'info9.html')
 
+
 def info_10(request):
     return render(request, 'info10.html')
-    
+
+
+def faculties(request):
+    return render(request, 'facultatives.html')
+
+
 def faculties_1(request):
 
     facultatives = Facultative.objects.all()
 
-    return render(request, 'facultatives.html', {'facultatives': facultatives})
+    return render(request, 'facultatives1.html', {'facultatives': facultatives})
+
 
 def faculties_2(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives2.html', {'facultatives': facultatives})
+
 
 def faculties_3(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives3.html', {'facultatives': facultatives})
+
 
 def faculties_4(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives4.html', {'facultatives': facultatives})
+
 
 def faculties_5(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives5.html', {'facultatives': facultatives})
+
 
 def faculties_6(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives6.html', {'facultatives': facultatives})
+
 
 def faculties_7(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives7.html', {'facultatives': facultatives})
+
 
 def faculties_8(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives8.html', {'facultatives': facultatives})
+
 
 def faculties_9(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives9.html', {'facultatives': facultatives})
+
 
 def faculties_10(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives10.html', {'facultatives': facultatives})
+
 
 def faculties_11(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives11.html', {'facultatives': facultatives})
+
 
 def faculties_12(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives12.html', {'facultatives': facultatives})
+
 
 def faculties_13(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives13.html', {'facultatives': facultatives})
+
 
 def faculties_14(request):
-    return render(request, 'faculties.html')
+    facultatives = Facultative.objects.all()
+
+    return render(request, 'facultatives14.html', {'facultatives': facultatives})
+
 
 def calendar_1(request):
     return render(request, 'calendar.html')
 
+
 def calendar_2(request):
     return render(request, 'calendar.html')
+
 
 def calendar_3(request):
     return render(request, 'calendar.html')
 
+
 def calendar_4(request):
     return render(request, 'calendar.html')
+
 
 def calendar_5(request):
     return render(request, 'calendar.html')
 
+
 def calendar_6(request):
     return render(request, 'calendar.html')
+
 
 def calendar_7(request):
     return render(request, 'calendar.html')
 
+
 def calendar_8(request):
     return render(request, 'calendar.html')
 
+
 def calendar_9(request):
     return render(request, 'calendar.html')
+
 
 def calendar_10(request):
     return render(request, 'calendar.html')
