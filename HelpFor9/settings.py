163b8ae19ce5c0268189e5ad50d9ef4edbba52e1
Evgeny <startsev2000@outlook.com>
2020-02-12 20:48:34 +0300

    Добавлен фикс названий папок

--- HelpFor9/settings.py
+++ HelpFor9/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kwldb%t9yu==mf+kvc1)b1ndxth&sdvuozc$hprh@&m+0hr2pm'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
