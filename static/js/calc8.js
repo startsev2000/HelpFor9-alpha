a5ff9c9a6fcc7bdc4bf1d7c50684b86784ba6a7a
Evgeny <startsev2000@outlook.com>
2020-02-28 22:40:50 +0300

    Полный фикс перед внедрением мобильной версии

--- static/js/calc8.js
+++ static/js/calc8.js
@@ -1,15 +1,13 @@
 function count() {
     let result = 0;
-    console.group();
     for (let i = 1; i <= 8; i++) {
-        console.log(typeof document.getElementById('n' + i).value.replace(',', '.'));
         if (parseFloat(document.getElementById('n' + i).value.replace(',', '.')) > 5 || parseFloat(document.getElementById('n' + i).value.replace(',', '.')) < 0) {
             alert('Неправильные данные. Средний балл должен быть меньше пяти, но больше нуля.');
             result = 0;
             break;
         }
         else {
-            result = parseFloat(document.getElementById('n' + i).value.replace(',', '.'));
+            result += parseFloat(document.getElementById('n' + i).value.replace(',', '.'));
         }
     }
     result /= 8;
